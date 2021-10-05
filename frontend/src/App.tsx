f92ce6ce34535e4e4c17a117b08626bf4aba6f13
Devin Lu <ludevinl@sfu.ca>
2021-10-04 18:26:50 -0700

    configured base react code and structure, configured to access endpoints from server

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,25 +1,32 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import Home from './Components/Home/Home'
+import Login from './Components/Login/Login'
+import Employees from './Components/Employees/Employees'
+
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+                <Router>
+                    {/* <Navbar /> */}
+                    <Switch>
+                        <Route exact path="/">
+                          <Home />
+                        </Route>
+
+                        <Route path="/login">
+                          <Login />  
+                        </Route>
+
+                        <Route path="/employees">
+                          <Employees />  
+                        </Route>
+                    </Switch>
+                </Router>
+            </>
   );
 }
 
