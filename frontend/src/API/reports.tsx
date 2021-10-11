4a0726763333a86894a49068661f1f1bfc554858
Zeti Xiong <zetix@sfu.ca>
2021-10-11 07:17:16 -0700

    Set up add button for adding record entries

--- frontend/src/API/reports.tsx
+++ frontend/src/API/reports.tsx
@@ -2,19 +2,23 @@ import axios from 'axios'
 
 const BASEURL = 'http://localhost:8080/api/reports/'
 
-function newReport(department: string) {
+export function newReport(department: string) {
     return axios.post(BASEURL, {"department":department}).then(res => res.data)
 }
 
-function getReport(id: number) {
+export function getReport(id: number) {
     return axios.get(BASEURL + id).then(res => res.data)
 }
 
-function addQuestion(id: number, question: string) {
+export function addEmptyQuestion(id: number) {
+    return axios.post(BASEURL + id + '/questions').then(res => res.data.questions)
+}
+
+export function addQuestion(id: number, question: string) {
     return axios.post(BASEURL + id + '/questions', {"question":question}).then(res => res.data.questions)
 }
 
-function updateQuestion(id: number, question: string, answer: string) {
+export function updateQuestion(id: number, question: string, answer: string) {
     return axios.put(BASEURL + 'questions/' + id, {"question":question, "answer":answer}).then(res => res.data)
 }
 
--- frontend/src/Components/Departments/DataInput.tsx
+++ frontend/src/Components/Departments/DataInput.tsx
@@ -1,10 +1,12 @@
 import React from 'react';
-import {useState} from "react";
+import Button from '@mui/material/Button';
 import Grid from '@mui/material/Grid';
 import TextField from '@mui/material/TextField';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import Divider from '@mui/material/Divider';
+import AddIcon from '@mui/icons-material/Add';
+import {newReport, addEmptyQuestion} from '../../API/reports'
 
 enum RecordType {
     written,
@@ -21,6 +23,11 @@ type EntryState = {
     question: string;
     answer: string;
     type: RecordType;
+};
+
+type RecordState = {
+    id: number;
+    entryList: RecordEntry[];
 };
 
 class RecordEntry extends React.Component<Props, EntryState> {
@@ -42,10 +49,10 @@ class RecordEntry extends React.Component<Props, EntryState> {
                 <ListItem alignItems="flex-start">
                     <Grid container spacing={2}>
                         <Grid item xs={8}>
-                            <TextField fullWidth id="field" label="Field" variant="outlined"/>
+                            <TextField fullWidth id="field" label="Field" variant="outlined" defaultValue={this.state.question}/>
                         </Grid>
                         <Grid item xs={4}>
-                            <TextField fullWidth id="value" label="Value" variant="outlined"/>
+                            <TextField fullWidth id="value" label="Value" variant="outlined" defaultValue={this.state.answer}/>
                         </Grid>
                     </Grid>
                 </ListItem>
@@ -55,15 +62,47 @@ class RecordEntry extends React.Component<Props, EntryState> {
     }
 }
 
-function DataInput() {
-    return (
-        <div className="DataInput">
-            <h1>Department Data Input</h1>
-            <List>
+class DataInput extends React.Component {
+    state: RecordState = {
+        entryList: [],
+        id: 0
+    }
 
-            </List>
-        </div>
-    )
+    constructor(props: any) {
+        super(props)
+        newReport(props.department).then(r => this.state.id = r.id)
+    }
+
+    newEntry(id: number) {
+        return <RecordEntry id={id} type={RecordType.written}/>
+    }
+
+    createNewEntry() {
+        addEmptyQuestion(this.state.id).then(r => {
+            let entry: any = this.newEntry(r.id)
+            this.state.entryList.push(entry)
+        })
+    }
+
+    render() {
+        return (
+            <div className="DataInput">
+                <h1>Department Data Input</h1>
+                <List>
+                    {this.state.entryList.map(function(entry: RecordEntry, index: number) {
+                        return {entry}
+                    })}
+                    <ListItem>
+                        <Button variant="contained" size="large" startIcon={<AddIcon fontSize="large"/>} onClick={() => {
+                            this.createNewEntry();
+                        }}>Add</Button>
+                    </ListItem>
+                </List>
+            </div>
+        )
+    }
+
+
 }
 
 export default DataInput;
