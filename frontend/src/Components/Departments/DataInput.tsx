39a1cc4d0e3de9891f2cade7f14610e0c5ef0b65
Zeti Xiong <zetix@sfu.ca>
2021-10-11 04:41:47 -0700

    Add record entry class

--- frontend/src/Components/Departments/DataInput.tsx
+++ frontend/src/Components/Departments/DataInput.tsx
@@ -1,6 +1,22 @@
 import React from 'react';
 import {useState} from "react";
 
+enum RecordType {
+    written,
+    MCQ,
+}
+
+class RecordEntry {
+    question: string = "";
+    answer: string = "";
+    type: RecordType = RecordType.written
+    private id: number;
+
+    constructor(id: number) {
+        this.id = id
+    }
+}
+
 function DataInput() {
     const [fieldList, setFieldList] = useState([{ firstName: "", lastName: "" }]);
 
