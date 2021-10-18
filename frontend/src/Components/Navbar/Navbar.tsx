ecfd61fa72ad993640367a3fb3f531bd5db88046
Alex-PC <mql@sfu.ca>
2021-10-18 16:38:33 -0700

    - Loging in: no longer shows navbar. - Navbar: removed login

--- frontend/src/Components/Navbar/Navbar.tsx
+++ frontend/src/Components/Navbar/Navbar.tsx
@@ -1,60 +1,85 @@
 import React from "react";
-import { Link } from 'react-router-dom'
-import '../../Departments.css'
+import { Link } from "react-router-dom";
+import "../../Departments.css";
 
 class Navbar extends React.Component {
-    render() {
-        return (
-            <div className="navbar-dark bg-dark shadow">
-                <div className="container">
-                    <div className="row">
-                        <div className="col-md-12">
-                        <nav className="navbar navbar-expand-lg">
-                            <div className="container-fluid">
-                                <Link to="/" className="navbar-brand">Hope Health Action</Link>
-                                <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                                    <span className="navbar-toggler-icon"></span>
-                                </button>
-                                <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
-                                        <li className="nav-item">
-                                            <Link to="/" className="nav-link active">Home</Link>
-                                        </li>
-                                        <li className="nav-item">
-                                            <Link to="/login" className="nav-link active">Login</Link>
-                                        </li>
-                                        <li className="nav-item">
-                                            <Link to="/employees" className="nav-link active">Employees</Link>
-                                        </li>
-                                        <li className="nav-item">
-                                            <Link to="/maternity" className="nav-link active">Maternity</Link>
-                                        </li>
-                                        <li className="nav-item">
-                                            <Link to="/rehab" className="nav-link active">Rehab</Link>
-                                        </li>
-                                        <li className="nav-item">
-                                            <Link to="/nicu-paed" className="nav-link active">Nicu Paed</Link>
-                                        </li>
-                                        <li className="nav-item">
-                                            <Link to="/communityhealth" className="nav-link active">CommunityHealth</Link>
-                                        </li>
-                                        <li className="nav-item">
-                                            <Link to="/messages" className="nav-link active">messages</Link>
-                                        </li>
-                                        <li className="nav-item">
-                                            <Link to="/leaderboard" className="nav-link active">Leaderboard</Link>
-                                        </li>
-
-                                    </ul>
-                                </div>
-                            </div>
-                        </nav>
-                        </div>
-                    </div>
+  render() {
+    return (
+      <div className="navbar-dark bg-dark shadow">
+        <div className="container">
+          <div className="row">
+            <div className="col-md-12">
+              <nav className="navbar navbar-expand-lg">
+                <div className="container-fluid">
+                  <Link to="/" className="navbar-brand">
+                    Hope Health Action
+                  </Link>
+                  <button
+                    className="navbar-toggler"
+                    type="button"
+                    data-bs-toggle="collapse"
+                    data-bs-target="#navbarSupportedContent"
+                    aria-controls="navbarSupportedContent"
+                    aria-expanded="false"
+                    aria-label="Toggle navigation"
+                  >
+                    <span className="navbar-toggler-icon"></span>
+                  </button>
+                  <div
+                    className="collapse navbar-collapse"
+                    id="navbarSupportedContent"
+                  >
+                    <ul className="navbar-nav me-auto mb-2 mb-lg-0">
+                      <li className="nav-item">
+                        <Link to="/" className="nav-link active">
+                          Home
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link to="/employees" className="nav-link active">
+                          Employees
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link to="/maternity" className="nav-link active">
+                          Maternity
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link to="/rehab" className="nav-link active">
+                          Rehab
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link to="/nicu-paed" className="nav-link active">
+                          Nicu Paed
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link to="/communityhealth" className="nav-link active">
+                          CommunityHealth
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link to="/messages" className="nav-link active">
+                          messages
+                        </Link>
+                      </li>
+                      <li className="nav-item">
+                        <Link to="/leaderboard" className="nav-link active">
+                          Leaderboard
+                        </Link>
+                      </li>
+                    </ul>
+                  </div>
                 </div>
+              </nav>
             </div>
-        )
-    }
+          </div>
+        </div>
+      </div>
+    );
+  }
 }
 
 export default Navbar;
