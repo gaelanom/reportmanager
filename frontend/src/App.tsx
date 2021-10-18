dbf6bf353402f0a129cf6d68552cdec375955b5c
Alex-PC <mql@sfu.ca>
2021-10-18 16:22:06 -0700

    - Login: added bootstrap styling

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,17 +1,17 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-import Home from './Components/Home/Home'
-import Login from './Components/Login/Login'
-import Employees from './Components/Employees/Employees'
-import Navbar from './Components/Navbar/Navbar'
-import Maternity from './Components/Departments/Maternity'
-import Rehab from './Components/Departments/Rehab'
-import NicuPaed from './Components/Departments/NicuPaed'
-import CommunityHealth from './Components/Departments/CommunityHealth'
-import DataInput from './Components/Departments/DataInput'
-import Messages from './Components/Messages/Messages'
-import Leaderboard from './Components/Leaderboard/Leaderboard'
+import React from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Home from "./Components/Home/Home";
+import Login from "./Components/Login/Login";
+import Employees from "./Components/Employees/Employees";
+import Navbar from "./Components/Navbar/Navbar";
+import Maternity from "./Components/Departments/Maternity";
+import Rehab from "./Components/Departments/Rehab";
+import NicuPaed from "./Components/Departments/NicuPaed";
+import CommunityHealth from "./Components/Departments/CommunityHealth";
+import DataInput from "./Components/Departments/DataInput";
+import Messages from "./Components/Messages/Messages";
+import Leaderboard from "./Components/Leaderboard/Leaderboard";
 
 import {
   BrowserRouter as Router,
@@ -24,12 +24,10 @@ import {
 function App() {
   return (
     <>
-
       <Router>
         <Navbar />
         <Redirect to="/login" />
         <Switch>
-
           <Route exact path="/">
             <Home />
           </Route>
@@ -81,8 +79,6 @@ function App() {
           <Route path="/leaderboard">
             <Leaderboard />
           </Route>
-
-
         </Switch>
       </Router>
     </>
