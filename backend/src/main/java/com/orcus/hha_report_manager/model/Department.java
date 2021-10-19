10a7b8e44a2f490f3bfd99255479407a485193bd
Gaelan O'Shea-McKay <gosheamc@sfu.ca>
2021-10-18 18:10:26 -0700

    fixes #58, #60, #63

--- backend/src/main/java/com/orcus/hha_report_manager/model/Department.java
+++ backend/src/main/java/com/orcus/hha_report_manager/model/Department.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.util.List;
 
 @Entity
-@Table(name = "departments", uniqueConstraints = @UniqueConstraint(name = "name", columnNames = {"name"}))
+@Table(name = "departments", uniqueConstraints = @UniqueConstraint(columnNames = {"name"}))
 public class Department {
 
     @Id
