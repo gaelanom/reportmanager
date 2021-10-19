fffdbbb4bf735ce5fa5c92f1fced0ef2158d1222
Alex-PC <mql@sfu.ca>
2021-10-18 17:45:34 -0700

    - Login:  set input min/max-width

--- frontend/src/API/Api.ts
+++ frontend/src/API/Api.ts
@@ -1,4 +1,7 @@
 import * as Employess from "./employees";
 import * as Authorization from "./Authorization";
 
+/**
+ * All API calls should be defined here for on line all import.
+ */
 export default { Employess, Authorization };
