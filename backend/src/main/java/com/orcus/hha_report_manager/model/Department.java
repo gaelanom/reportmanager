633b35a947a0d63cd767bc0cda117c34c5cf63d5
Gaelan O'Shea-McKay <gosheamc@sfu.ca>
2021-10-18 18:08:39 -0700

    some small changes to departments; unique name requirement, partial PUTs, blurb field. Also an incomplete readme.

--- backend/src/main/java/com/orcus/hha_report_manager/model/Department.java
+++ backend/src/main/java/com/orcus/hha_report_manager/model/Department.java
@@ -4,7 +4,7 @@ import javax.persistence.*;
 import java.util.List;
 
 @Entity
-@Table(name = "departments")
+@Table(name = "departments", uniqueConstraints = @UniqueConstraint(name = "name", columnNames = {"name"}))
 public class Department {
 
     @Id
@@ -14,16 +14,31 @@ public class Department {
     @Column(name = "name")
     private String name;
 
+    @Column(name = "blurb")
+    private String blurb;
+
     public Department() {
 
     }
 
-    public Department(String name) {
+    public Department(String name, String blurb) {
         this.name = name;
+        this.blurb = blurb;
+    }
+
+    public long getId() {
+        return id;
     }
 
     public String getName() { return this.name; }
 
     public void setName(String name) { this.name = name; }
 
+    public String getBlurb() {
+        return blurb;
+    }
+
+    public void setBlurb(String blurb) {
+        this.blurb = blurb;
+    }
 }
