cab5b0680ab960755e9a4389c180a6a3f7d95a94
Alex-PC <mql@sfu.ca>
2021-11-15 17:37:29 -0800

    - Added: Authenticate now returns department ID.

--- backend/src/main/java/com/orcus/hha_report_manager/controller/AuthenticationController.java
+++ backend/src/main/java/com/orcus/hha_report_manager/controller/AuthenticationController.java
@@ -1,5 +1,7 @@
 package com.orcus.hha_report_manager.controller;
 
+import com.orcus.hha_report_manager.model.Employee;
+import com.orcus.hha_report_manager.repository.DepartmentRepository;
 import com.orcus.hha_report_manager.repository.EmployeeRepository;
 import com.orcus.hha_report_manager.security.SignedJwt;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,13 +10,15 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.AuthenticationException;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.core.userdetails.UsernameNotFoundException;
-import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.ResponseStatus;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class AuthenticationController {
@@ -28,6 +32,9 @@ public class AuthenticationController {
     @Autowired
     private EmployeeRepository employeeRepository;
 
+    @Autowired
+    private DepartmentRepository departmentRepository;
+
 
     @PostMapping("/authenticate")
     public ResponseEntity<AuthResponse> authenticate(@RequestBody AuthRequest authRequest) throws Exception {
@@ -36,17 +43,26 @@ public class AuthenticationController {
                     new UsernamePasswordAuthenticationToken(
                             authRequest.getUsername(),
                             authRequest.getPassword()));
-            var userDetails = userDetailsService.loadUserByUsername(authRequest.getUsername());
-            var employee = employeeRepository.findByUsername(userDetails.getUsername());
-            if (employee.isEmpty())
-                throw new UsernameNotFoundException(userDetails.getUsername());
 
-            return new ResponseEntity<>(new AuthResponse(SignedJwt.make(userDetails), employee.get(0).getDepartment()),
-                    HttpStatus.OK);
+            return new ResponseEntity<>(makeAuthResponse(authRequest.username), HttpStatus.OK);
         } catch (AuthenticationException e) {
             return new ResponseEntity<>(null, HttpStatus.UNAUTHORIZED);
         }
 
+    }
+
+    private AuthResponse makeAuthResponse(String username) {
+        var userDetails = userDetailsService.loadUserByUsername(username);
+        var employees = employeeRepository.findByUsername(userDetails.getUsername());
+        if (employees.isEmpty())
+            throw new UsernameNotFoundException(userDetails.getUsername());
+        var departments = departmentRepository.findByName(employees.get(0).getDepartment());
+        if (departments.isEmpty())
+            throw new UsernameNotFoundException(userDetails.getUsername());
+
+        return new AuthResponse(SignedJwt.make(userDetails),
+                employees.get(0).getDepartment(),
+                departments.get(0).getId());
     }
 
     @ResponseStatus(code = HttpStatus.UNAUTHORIZED)
@@ -85,6 +101,7 @@ public class AuthenticationController {
     public static class AuthResponse {
         private final String jwt;
         private final String department;
+        private final long departmentId;
 
         public String getJwt() {
             return jwt;
@@ -94,9 +111,14 @@ public class AuthenticationController {
             return department;
         }
 
-        public AuthResponse(String jwt, String department) {
+        public long getDepartmentId() {
+            return departmentId;
+        }
+
+        public AuthResponse(String jwt, String department, long departmentId) {
             this.jwt = jwt;
             this.department = department;
+            this.departmentId = departmentId;
         }
     }
 }
