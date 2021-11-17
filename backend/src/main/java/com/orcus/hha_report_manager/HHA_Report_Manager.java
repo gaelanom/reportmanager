340e1174af6b21e1de1f51a27349bf8a0abfea72
Alex-PC <mql@sfu.ca>
2021-11-17 14:32:53 -0800

    - Moved: initialization logic to a component

--- backend/src/main/java/com/orcus/hha_report_manager/HHA_Report_Manager.java
+++ backend/src/main/java/com/orcus/hha_report_manager/HHA_Report_Manager.java
@@ -16,44 +16,7 @@ import java.util.List;
 
 @SpringBootApplication
 public class HHA_Report_Manager {
-
 	public static void main(String[] args) {
 		SpringApplication.run(HHA_Report_Manager.class, args);
 	}
-
-	@Autowired
-	private EmployeeRepository employeeRepository;
-
-	@Autowired
-	private DepartmentRepository departmentRepository;
-
-	@Autowired
-	private EmployeeController employeeController;
-
-	@EventListener(ApplicationReadyEvent.class)
-	public void initializeSomething() {
-		initDefaultEmployees();
-	}
-
-	private void initDefaultEmployees() {
-		List<Employee> defaultEmployees = makeDefaultEmployees();
-		for (var employee : defaultEmployees) {
-			var employeeList = employeeRepository.findByUsername(employee.getUsername());
-			if (employeeList.isEmpty())
-				employeeController.createEmployee(employee);
-		}
-	}
-
-	private List<Employee> makeDefaultEmployees() {
-		final String DEFAULT_PW = "12345";
-		final String nicu = "NICU";
-		final Employee DEV = new Employee("dev", DEFAULT_PW, "", "", nicu, true);
-		if(departmentRepository.findByName(nicu).isEmpty())
-			departmentRepository.save(new Department(nicu,""));
-
-		final Employee ADMIN = new Employee("admin", DEFAULT_PW, "", "", "ADMIN", true);
-		ADMIN.setAdmin(true);
-		return List.of(DEV, ADMIN);
-	}
-
 }
