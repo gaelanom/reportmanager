f3ae6852061913f49b01476ff0e659e523fd90f4
Devin Lu <ludevinl@sfu.ca>
2021-11-17 21:04:10 -0800

    added modal

--- frontend/src/Components/Departments/Departments.tsx
+++ frontend/src/Components/Departments/Departments.tsx
@@ -38,7 +38,7 @@ class Departments extends React.Component<any, any> {
                 <h1 className="card-header card-title text-center display-4">Departments</h1>
                 <div className="card-body">
                 <div className="d-grid gap-2 col-6 mx-auto mb-3">
-                    <button className="btn btn-primary" type="button">Add New Department</button>
+                    <button className="btn btn-primary" type="button" data-bs-toggle="modal" data-bs-target="#add-departments-modal">Add New Department</button>
                 </div>
                 
                 <ul className="list-group">
@@ -61,6 +61,28 @@ class Departments extends React.Component<any, any> {
                             )
                     })}
                 </ul>
+                </div>
+            </div>
+
+            {/* Modal */}
+            <div className="modal fade" id="add-departments-modal" aria-labelledby="modal-label" aria-hidden="true">
+                <div className="modal-dialog">
+                    <div className="modal-content">
+                    <div className="modal-header">
+                        <h5 className="modal-title" id="modal-label">Create New Department</h5>
+                        <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+                    </div>
+                    <div className="modal-body">
+                        <div className="mb-3">
+                            <label htmlFor="department-name-input" className="form-label">Password</label>
+                            <input className="form-control" id="department-name-input" />
+                        </div>
+                    </div>
+                    <div className="modal-footer">
+                        <button type="button" className="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+                        <button type="button" className="btn btn-primary">Submit</button>
+                    </div>
+                    </div>
                 </div>
             </div>
             </>
