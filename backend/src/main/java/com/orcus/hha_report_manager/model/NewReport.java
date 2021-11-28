df78f2193e9bebc7589c48bd87b333cd0aa0b370
gaelanom <gaelanosheamckay@hotmail.com>
2021-11-27 19:57:32 -0800

    removed unique constraint

--- backend/src/main/java/com/orcus/hha_report_manager/model/NewReport.java
+++ backend/src/main/java/com/orcus/hha_report_manager/model/NewReport.java
@@ -6,7 +6,7 @@ import java.util.List;
 import java.util.Map;
 
 @Entity
-@Table(name = "newReports", uniqueConstraints = @UniqueConstraint(columnNames = {"month", "departmentId"}))
+@Table(name = "newReports")
 public class NewReport {
 
     @Id
